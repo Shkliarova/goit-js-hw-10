6ed4bac2ac525bb9c3f064df9da9ce8e2b4f9445
Shkliarova <shklyarovaalisa@gmail.com>
2023-09-26 22:45:57 +0300

    hw-part2

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,7 +1,40 @@
+import { breedSelect, catInfo, createMarkup } from "./index.js";
+
 export function fetchBreeds(){
-    console.log('hello');
+    return fetch('https://api.thecatapi.com/v1/breeds')
+    .then(response => {
+        if(!response.ok){
+            throw new Error(response.status);
+        }
+        return response.json();
+    })
+    .then(data => {
+        data.forEach(breed => {
+            const option = document.createElement('option');
+            option.value = breed.id;
+            option.textContent = breed.name;
+            breedSelect.appendChild(option);
+          })
+          return data;
+    })
+    .catch(error => {
+        console.log(error);
+    })
 }
 
 export function fetchCatByBreed(breedId){
-    console.log('hello');
+    return fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    .then(response => {
+        if(!response.ok){
+            throw new Error(response.status);
+        }
+        return response.json();
+    })
+    .then(result => {
+        const markup = createMarkup(result);
+        catInfo.innerHTML = markup;
+    })
+    .catch(error => {
+        console.log(error);
+    })
 }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,29 @@ import axios from "axios";
 axios.defaults.headers.common["x-api-key"] = "live_dOJ39ImEqWH3cF1jUJI4PMe7tAnZXHOkINz33Hfe1NJ2IErVO1ESFhxy6AB6WCAO";
 import {fetchBreeds, fetchCatByBreed} from './cat-api'
 
-const breedSelect = document.querySelector('.breed-select');
+export const breedSelect = document.querySelector('.breed-select');
 breedSelect.style.width = '200px';
 breedSelect.style.height = '25px';
 
 const infoLoader = document.querySelector('.loader');
 const selectError = document.querySelector('.error');
-const catInfo = document.querySelector('.cat-info');
+export const catInfo = document.querySelector('.cat-info');
 
-breedSelect.addEventListener('change', fetchCatByBreed);
-window.addEventListener('load', fetchBreeds);
+window.addEventListener('load', init);
+
+function init(){
+    fetchBreeds();
+    breedSelect.addEventListener('change', () => {
+        const selectBreedId = breedSelect.value;
+        fetchCatByBreed(selectBreedId);
+    });
+}
+
+export function createMarkup(arr){
+    return arr.map(({url, name, description, temperament}) => {
+        return `<img src='${url}' width='300' alt='${name}'/>
+        <h1>${name}</h1>
+        <p>${description}</p>
+        <p><b>Temperament:</b> ${temperament}</p>`
+    }).join('');
+}
