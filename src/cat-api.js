96c75b597adb3ba9684ac8fbd1785a19a9662188
Shkliarova <shklyarovaalisa@gmail.com>
2023-09-27 22:28:25 +0300

    hw-part4

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,40 +1,41 @@
-import { breedSelect, catInfo, createMarkup } from "./index.js";
+import { infoLoader, selectError, breedSelect, catInfo } from "./index.js";
+const api_key = "live_dOJ39ImEqWH3cF1jUJI4PMe7tAnZXHOkINz33Hfe1NJ2IErVO1ESFhxy6AB6WCAO";
 
 export function fetchBreeds(){
-    return fetch('https://api.thecatapi.com/v1/breeds')
+    breedSelect.style.display = 'none';
+    infoLoader.style.display = 'block';
+    return fetch(`https://api.thecatapi.com/v1/breeds?api_key=${api_key}`)
     .then(response => {
         if(!response.ok){
             throw new Error(response.status);
         }
         return response.json();
     })
-    .then(data => {
-        data.forEach(breed => {
-            const option = document.createElement('option');
-            option.value = breed.id;
-            option.textContent = breed.name;
-            breedSelect.appendChild(option);
-          })
-          return data;
-    })
     .catch(error => {
         console.log(error);
+        selectError.style.display = 'block';
+    })
+    .finally(() => {
+        infoLoader.style.display = 'none';
+        breedSelect.style.display = 'block';
     })
 }
 
 export function fetchCatByBreed(breedId){
-    return fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    catInfo.style.display = 'none';
+    return fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&api_key=${api_key}`)
     .then(response => {
         if(!response.ok){
             throw new Error(response.status);
         }
         return response.json();
     })
-    .then(result => {
-        const markup = createMarkup(result);
-        catInfo.innerHTML = markup;
-    })
     .catch(error => {
         console.log(error);
+        selectError.style.display = 'block';
+    })
+    .finally(() => {
+        infoLoader.style.display = 'none';
+        catInfo.style.display = 'flex';
     })
 }
--- src/index.js
+++ src/index.js
@@ -3,28 +3,45 @@ axios.defaults.headers.common["x-api-key"] = "live_dOJ39ImEqWH3cF1jUJI4PMe7tAnZX
 import {fetchBreeds, fetchCatByBreed} from './cat-api'
 
 export const breedSelect = document.querySelector('.breed-select');
-breedSelect.style.width = '200px';
-breedSelect.style.height = '25px';
 
-const infoLoader = document.querySelector('.loader');
-const selectError = document.querySelector('.error');
+export const infoLoader = document.querySelector('.loader');
+export const selectError = document.querySelector('.error');
 export const catInfo = document.querySelector('.cat-info');
 
 window.addEventListener('load', init);
 
 function init(){
-    fetchBreeds();
+    selectError.style.display = 'none';
+    infoLoader.style.display = 'none';
+    let breedsData;
+    fetchBreeds()
+    .then(data => {
+        breedsData = data;
+        data.forEach(breed => {
+            const option = document.createElement('option');
+            option.value = breed.id;
+            option.textContent = breed.name;
+            breedSelect.appendChild(option);
+          })
+    })
     breedSelect.addEventListener('change', () => {
         const selectBreedId = breedSelect.value;
-        fetchCatByBreed(selectBreedId);
+        infoLoader.style.display = 'block';
+
+        fetchCatByBreed(selectBreedId)
+        .then(result => {
+            const catData = result[0];
+            const breedData = breedsData.find(breed => breed.id === catData.breeds[0].id);
+
+            const markup = createMarkup(catData, breedData);
+            catInfo.innerHTML = markup;
+        })
     });
 }
 
-export function createMarkup(arr){
-    return arr.map(({url, name, description, temperament}) => {
-        return `<img src='${url}' width='300' alt='${name}'/>
-        <h1>${name}</h1>
-        <p>${description}</p>
-        <p><b>Temperament:</b> ${temperament}</p>`
-    }).join('');
-}
+        function createMarkup(catData, breedData){
+        return `<img src='${catData.url}' width='300' alt='${breedData.name}'/>
+        <div class='textInfo'><h1>${breedData.name}</h1>
+        <p>${breedData.description}</p>
+        <p><b>Temperament:</b> ${breedData.temperament}</p></div>`
+    }
